9259b433b0db43e19d1b70dd6429ee7d9271534a
toptecshare <eestimorol.ice9208g@gmail.com>
2022-02-17 05:25:07 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,406 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="page-wrap">
+      <div
+        data-animation="over-left"
+        data-collapse="medium"
+        data-duration="400"
+        data-easing="ease"
+        data-easing2="ease"
+        role="banner"
+        className="side-navigation w-nav"
+      >
+        <div className="nav-wrap w-container">
+          <div className="mobile-header-cover">
+            <div className="cyber-corner tm">
+              <img
+                src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fc443fb3754e693da12dba_Cyber%20Corner%20TM3.png"
+                loading="lazy"
+                alt=""
+                className="cyber-mobile-corner"
+              />
+            </div>
+            <div className="bottom-line"></div>
+          </div>
+          <div className="brand-wrap">
+            <div
+              data-w-id="69d87527-4842-2a7d-7bec-5f0c10e284cf"
+              className="brand w-nav-brand"
+              aria-label="home"
+            >
+              <img
+                src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61f6d4481502cfd8afe92c3e_Cyber%20Snail%20Animated%20Logotype%20Crop.gif"
+                loading="lazy"
+                alt=""
+                className="side-logotype"
+              />
+              <div className="logo-fade"></div>
+              <div className="logo-fade mobile-fade"></div>
+            </div>
+          </div>
+          <nav
+            role="navigation"
+            data-w-id="69d87527-4842-2a7d-7bec-5f0c10e284d1"
+            className="side-nav-links w-nav-menu"
+          >
+            <div className="menu-lable">Menu</div>
+            <div className="cyber-corner tr-mobile">
+              <img
+                src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fc09f6309b3e746a4855dd_Cyber%20Corner%20Top%20Right.png"
+                loading="lazy"
+                alt=""
+                className="cyber-corner-tr1"
+              />
+            </div>
+            <div
+              data-w-id="69d87527-4842-2a7d-7bec-5f0c10e284ce"
+              className="nav-link w-nav-link max-width"
+            >
+              Connect
+            </div>
+            <div className="separator"></div>
+            <div
+              data-w-id="69d87527-4842-2a7d-7bec-5f0c10e284d2"
+              className="nav-link w-nav-link max-width"
+            >
+              Cyber Snail
+            </div>
+            <div
+              data-w-id="69d87527-4842-2a7d-7bec-5f0c10e284d6"
+              aria-current="page"
+              className="nav-link w-nav-link w--current max-width"
+            >
+              Cyber Key
+            </div>
+            <div
+              data-w-id="69d87527-4842-2a7d-7bec-5f0c10e284d8"
+              className="nav-link w-nav-link max-width"
+            >
+              Total $UPLOAD
+            </div>
+            <div
+              data-w-id="69d87527-4842-2a7d-7bec-5f0c10e284da"
+              className="nav-link w-nav-link max-width"
+            >
+              Upload
+            </div>
+
+            <div target="_blank" className="nav-link w-nav-link max-width">
+              Claim
+            </div>
+            <div className="social-nav left-social">
+              <div className="social-icon-wrapper">
+                <div target="_blank" className="social-icon w-inline-block">
+                  <div className="svg-icon w-embed">
+                    <svg
+                      viewBox="0 0 19 16"
+                      width="60"
+                      fill="CurrentColor"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path d="M18.5057 0.276596C18.5057 0.276596 16.924 1.08511 16.0343 1.2766C15.3424 0.468085 14.3143 0 13.1873 0C10.6764 0 8.89698 2.40426 9.39126 5.02128C6.16857 4.82979 3.24246 3.21277 1.34443 0.808511C1.00833 1.42553 0.850158 2.10638 0.830387 2.80851C0.830387 4.21277 1.52237 5.48936 2.55047 6.14894C1.9178 6.14894 1.30489 5.95745 0.771074 5.61702C0.751302 7.57447 2.09573 9.29788 3.87513 9.6383C3.30177 9.76596 2.6691 9.85107 2.09573 9.70213C2.37253 10.5319 2.86681 11.234 3.51925 11.7447C4.1717 12.2553 4.96254 12.5106 5.77316 12.5106C4.21124 13.766 1.95734 14.5958 0 14.1915C3.79605 16.7234 8.99584 16.617 12.6337 13.7447C15.5598 11.4468 17.0427 7.78724 17.0427 3.95745C17.794 3.38298 18.4464 2.68085 19 1.89362C18.308 2.23404 17.5369 2.42553 16.7856 2.55319C17.5567 2 18.1894 1.2766 18.5057 0.276596Z"></path>
+                    </svg>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="cyber-corner bl-mobile">
+              <img
+                src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fc09f6bce6dcdda3b7b657_Cyber%20Corner%20BL1%401000x.png"
+                loading="lazy"
+                alt=""
+                className="cyber-corner-bl1"
+              />
+            </div>
+          </nav>
+          <div
+            data-w-id="33cc1502-eb9e-2c24-154d-0202e2244e4f"
+            className="menu-button-2 w-nav-button webkit-user-select"
+            aria-label="menu"
+            role="button"
+            aria-controls="w-nav-overlay-0"
+            aria-haspopup="menu"
+            aria-expanded="false"
+          >
+            <div className="mobile-menu-lable">Menu</div>
+            <div className="mobile-menu-lable close-menu">Close</div>
+          </div>
+        </div>
+        <div
+          className="w-nav-overlay display-none"
+          data-wf-ignore=""
+          id="w-nav-overlay-0"
+        ></div>
+      </div>
+      <div className="section roadmap-section">
+        <div className="cyber-corner tl">
+          <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbd9b48af3ea96351d949f_Cyber%20Corner%20TL1%401000x.png"
+            loading="lazy"
+            alt=""
+            className="cyber-corner-tl1"
+          />
+        </div>
+        <div className="roadmap-progress"></div>
+        <div className="progress-line">
+          <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fc0765970e9a5dc4ad0a3a_P%20Line%203.svg"
+            loading="lazy"
+            className="style-opacity"
+            data-w-id="3a337499-1285-f848-f09d-15ad14b000d6"
+            alt=""
+          />
+        </div>
+        <div className="m-padding">
+          <div className="heading-wrap">
+            <div className="margin-bottom-m">
+              <h1
+                data-w-id="af08d695-b1e7-659a-18f3-300825529f31"
+                style={{ opacity: 1 }}
+                className="main-heading"
+              >
+                Roadmap
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="roadmap-section">
+          <div className="cyber-corner br">
+            <img
+              src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbffcad00adb0c44c6aa8e_Cyber%20Corner%20BR2%401000x.png"
+              loading="lazy"
+              data-w-id="ec7d0f6f-1089-d4d0-4aae-9133a5f98f2b"
+              alt=""
+              className="cyber-corner-br2 style-opacity"
+            />
+          </div>
+          <div className="roadmap-left">
+            <div
+              data-w-id="6cfc0fc7-8caf-277a-7dcc-9cf949653851"
+              className="roadmap-mark style-translate"
+            ></div>
+          </div>
+          <div className="roadmap-content">
+            <h1 className="roadmap-stage-title cyan-span">
+              The Portal Opens...
+            </h1>
+            <span>
+              Cyber Keys are distributed to those who solve the riddles. Only
+              1000 make it through.
+              <br />
+              <br />
+              Key holders choose who they want to bring with them during The
+              Upload - selecting specific blue-chip projects to let into the
+              locked Discord.
+              <br />
+            </span>
+          </div>
+          <div
+            id="w-node-_8885c16c-2a0d-3a83-a61b-e18b139c2839-218089bb"
+            className="roadmap-image-wrap"
+          >
+            <img
+              src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/620181df95485977715dc223_Card.png"
+              loading="lazy"
+              alt=""
+              className="roadmap-image"
+            />
+          </div>
+        </div>
+        <div className="roadmap-section alt-stage">
+          <div className="roadmap-left">
+            <div
+              data-w-id="37a6b5c9-1a08-086f-35f0-3cf86cc54c26"
+              className="roadmap-mark style-translate"
+            ></div>
+          </div>
+          <div></div>
+          <div
+            id="w-node-_37a6b5c9-1a08-086f-35f0-3cf86cc54c27-218089bb"
+            className="roadmap-content"
+          >
+            <span>
+              The Discord opens to the remaining public as The Upload
+              approaches. Cyber Key holders are secured a place with 2 whitelist
+              mints each along with future utility of 2x $UPLOAD accrual and
+              early alpha access.
+              <br />
+              <br />
+              Others join The Selected for 1 whitelist place each based on their
+              community contribution.
+            </span>
+          </div>
+          <div className="cyber-corner br">
+            <img
+              src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbffca1f7873cc7c47b319_Cyber%20Corner%20BR4%401000x.png"
+              loading="lazy"
+              alt=""
+              className="cyber-corner-br3"
+            />
+          </div>
+        </div>
+        <div className="roadmap-section main-stage">
+          <div className="cyber-corner tl">
+            <img
+              src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbd9b48af3ea96351d949f_Cyber%20Corner%20TL1%401000x.png"
+              loading="lazy"
+              alt=""
+              className="cyber-corner-tl1"
+            />
+          </div>
+          <div className="cyber-corner br" />
+          <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbffcad00adb0c44c6aa8e_Cyber%20Corner%20BR2%401000x.png"
+            loading="lazy"
+            alt=""
+            className="cyber-corner-br2"
+          />
+        </div>
+        <div className="roadmap-left">
+          <div
+            data-w-id="a40a422d-6775-b977-7d0c-eef12a19348e"
+            className="roadmap-mark style-translate"
+          ></div>
+        </div>
+        <div className="roadmap-content">
+          <h1 className="roadmap-stage-title pink-span">
+            The Upload Begins...
+          </h1>
+          <span>
+            The public launch of the Cyber Snails Collection will allow
+            whitelisted addresses to be minted first. Key holders will have
+            double mints available. Then the public will have their chance at
+            securing a Cyber Snail.
+            <br />
+          </span>
+        </div>
+        <div
+          id="w-node-_8727cb5d-7445-1ea5-1a19-2902016df570-218089bb"
+          className="roadmap-image-wrap"
         >
-          Learn React
-        </a>
-      </header>
+          <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/620181defb297b6c2ae2d321_Upload96.png"
+            loading="lazy"
+            alt=""
+            className="roadmap-image"
+          />
+        </div>
+      </div>
+      <div className="roadmap-section alt-stage">
+        <div className="roadmap-left">
+          <div
+            data-w-id="c6b0f620-7a90-01e4-a2c6-3a5378edb368"
+            className="roadmap-mark style-translate"
+          ></div>
+        </div>
+        <div></div>
+        <div
+          id="w-node-c6b0f620-7a90-01e4-a2c6-3a5378edb369-218089bb"
+          className="roadmap-content"
+        >
+          <span>
+            Once the upload has successfully completed, each Cyber Snail will
+            start yielding $UPLOAD daily and holders will have access to the
+            Snails Alpha Service, Alpha tools, Investment Fund and other utility
+            described in the whitepaper.
+            <br />
+            <br />
+            The investment fund run by the founders and team will take off and
+            the LP will receive consistent profits from the trades.
+            <br />
+          </span>
+        </div>
+        <div className="cyber-corner br">
+          <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbffca1f7873cc7c47b319_Cyber%20Corner%20BR4%401000x.png"
+            loading="lazy"
+            alt=""
+            className="cyber-corner-br3"
+          />
+        </div>
+      </div>
+      <div className="roadmap-section main-stage">
+        <div className="cyber-corner tl">
+          <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbd9b48af3ea96351d949f_Cyber%20Corner%20TL1%401000x.png"
+            loading="lazy"
+            alt=""
+            className="cyber-corner-tl1"
+          />
+        </div>
+        <div className="cyber-corner br">
+          <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbffcad00adb0c44c6aa8e_Cyber%20Corner%20BR2%401000x.png"
+            loading="lazy"
+            alt=""
+            className="cyber-corner-br2"
+          />
+        </div>
+        <div className="roadmap-left">
+          <div
+            data-w-id="3d5f783d-4af6-59e3-fa5b-f2dcc62db6f5"
+            className="roadmap-mark style-translate"
+          ></div>
+        </div>
+        <div className="roadmap-content">
+          <h1 className="roadmap-stage-title minty-span">
+            Building the Cyberverse...
+          </h1>
+          <span>
+            After the launch of the main Cyber Snail collection, the main focus
+            is running the investment fund, maturing the Snails Alpha Service
+            and building the Alpha Tools and Cyber Chip NFT&nbsp;drop along with
+            related utility.
+            <br />
+            <br />
+            Read more in the <div>whitepaper</div>
+            .<br />
+          </span>
+        </div>
+        <div
+          id="w-node-_9d72570c-d37a-49ac-6c8b-88afb007d5ef-218089bb"
+          className="roadmap-image-wrap"
+        >
+          {/* <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/6209701f833f49d36af16f82_ATM20edt.png"
+            loading="lazy"
+            sizes="(max-width: 479px) 6vw, (max-width: 991px) 17vw, 9vw"
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/6209701f833f49d36af16f82_ATM20edt-p-500.png 500w, https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/6209701f833f49d36af16f82_ATM20edt.png 1114w"
+            alt=""
+            className="roadmap-image-copy"
+          /> */}
+        </div>
+      </div>
+      <div className="roadmap-section alt-stage">
+        <div className="roadmap-left">
+          <div
+            data-w-id="03eee916-a00b-12e1-e00e-a56d18c699e5"
+            className="roadmap-mark style-translate"
+          ></div>
+        </div>
+        <div></div>
+        <div
+          id="w-node-_03eee916-a00b-12e1-e00e-a56d18c699e7-218089bb"
+          className="roadmap-content"
+        >
+          <span>
+            The project will continue and scale recruiting more talent to the
+            investment operations in order to build a consistent and collected
+            community of absolute printers.
+            <br />
+          </span>
+        </div>
+        <div className="cyber-corner br">
+          <img
+            src="https://uploads-ssl.webflow.com/61f6d21a35352d2f1d282e71/61fbffca1f7873cc7c47b319_Cyber%20Corner%20BR4%401000x.png"
+            loading="lazy"
+            alt=""
+            className="cyber-corner-br3"
+          />
+        </div>
+      </div>
     </div>
   );
 }
